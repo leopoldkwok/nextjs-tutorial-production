3030700cb142b5231318f0b74c63c08aa323fdef
leopoldkwok <leopoldkwok@gmail.com>
2023-11-27 17:27:57 +0000

    Remote Images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -22,7 +22,15 @@ const SingleDrinkPage = async ({ params }) => {
       <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
         back to drinks
       </Link>
-      <Image src={drinkImg} className='w-48 h-48 rounded-lg' alt='drink' />
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className='w-48 h-48 rounded-lg shadow-lg mb-4'
+        priority
+        alt={title}
+      />
+      {/* <Image src={drinkImg} className='w-48 h-48 rounded-lg' alt='drink' /> */}
       <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
   );
