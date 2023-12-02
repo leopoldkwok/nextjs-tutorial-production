f70991eef30b092ea5487f701f746f81e00bdd4f
leopoldkwok <leopoldkwok@gmail.com>
2023-12-02 22:42:26 +0000

    update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,16 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-const PrismaClientSingleton = () => {
+const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-type PrismaClientSingleton = ReturnType<typeof PrismaClientSingleton>;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? PrismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
